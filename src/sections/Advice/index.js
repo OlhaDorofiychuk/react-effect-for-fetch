afcb2162524e8c972d10585a84c491ec4616c3f0
OlhaDorofiychuk <olha.d@yahoo.com>
2022-11-16 15:32:44 +0000

    core advice

--- src/sections/Advice/index.js
+++ src/sections/Advice/index.js
@@ -1,13 +1,33 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function AdviceSection() {
+  const [advice, setAdvice] = useState({});
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/")
+      .then((res) => res.json())
+      .then((dataAdvice) => setAdvice(dataAdvice));
+  });
   return (
     <section>
       <h2>Advice Section</h2>
-      <section className="adivce-slip"></section>
-      <section className="favourtite-slips-list"></section>
+      <section className="adivce-slip">
+        <h3>Some Advice</h3>
+        <p>Always the burrito.</p>
+        <button>Get More Advice</button>
+        <button>Save to Favourties</button>
+      </section>
+
+      <section className="favourtite-slips-list">
+        <h3>Favourite Advice Slips</h3>
+        <ul>
+          <li>Measure twice, cut once.</li>
+          <li>Don't let the bastards grind you down.</li>
+          <li>Always the burrito.</li>
+        </ul>
+      </section>
     </section>
-  )
+  );
 }
 
-export default AdviceSection
+export default AdviceSection;
--- src/sections/Art/index.js
+++ src/sections/Art/index.js
@@ -1,12 +1,16 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function ArtsSection() {
+  // useEffect(() => {
+  //   fetch("")
+  //     .then((res) => res.json())
+  //     .then((dataAdvice) => setAdvice(dataAdvice));
   return (
     <section>
       <h2>Arts Section</h2>
       <div className="scroll-container"></div>
     </section>
-  )
+  );
 }
 
-export default ArtsSection
+export default ArtsSection;
